0c21feecc93bc0068097f363ee1b1d0932cdaae3
Eduardo Watanabe <eduardo.watanabe@vilt.local>
2024-06-12 00:06:45 -0300

    empiesando el projecto

---
+++ my-app-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hola que tal</h1>
+      <p>Este és mi primero projecto en React</p>
+    </div>
+  );
+}
+
+export default App;
