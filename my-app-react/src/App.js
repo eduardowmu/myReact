0dae834042dc7782751115d0de4e3ebd1b4a9ae6
Eduardo Watanabe <eduardo.watanabe@vilt.local>
2024-06-13 22:21:54 -0300

    Aula de JSX

--- my-app-react/src/App.js
+++ my-app-react/src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //podemos trabalhar também com JS, além de HTML, CSS, etc.
+  const name = 'Eduardo'
+
+  const imageURL = "https://via.placeholder.com/150"
+
+  function calculaIdade() {
+    const a = 1984
+    let b = new Date().getFullYear()
+    return b - a
+  }
+
   return (
     <div className="App">
       <h1>Hola que tal</h1>
       <p>Este és mi primero projecto en React</p>
+      <p>Mi nombre é {name}</p>
+      <p>Tenho {calculaIdade()} anos</p>
+      <img src={imageURL} alt=""/>
     </div>
   );
 }
