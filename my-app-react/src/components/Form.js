6ab46bd2b871495a55b2dcdab8f3c69bff105bda
Eduardo Watanabe <eduardo.watanabe@vilt.local>
2024-06-15 20:23:31 -0300

    trabalhando com eventos e useStates

--- my-app-react/src/components/Form.js
+++ my-app-react/src/components/Form.js
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 function Form() {
     function cadastrarContato(e) {
         /**
@@ -10,16 +12,34 @@ function Form() {
         let msg = window.confirm("Deseja cadastrar seu email?") ? 
                                         'e-mail enviado com sucesso!' :
                                         'e-mail não enviado.'
+        
+        console.log(email, password)
         alert(msg)
     }
+
+    //inserindo um valor default, como parametro
+    const [email, setEmail] = useState('dudu@gmail.com')
+    const [password, setPassword] = useState()
 
     return(
         <>
             <h1>Preencha o formulário</h1>
             <form onSubmit={cadastrarContato}>
                 <div>
-                    <input type="email" placeholder="exemplo fulano@gmail.com" 
-                                        required/>
+                    <label htmlFor="email">E-mail: </label>
+                    <input type="email" id="email" name="email" 
+                            placeholder="exemplo: fulano@gmail.com"
+                            value={email}
+                            onChange={(e => setEmail(e.target.value))}
+                            required/>
+                </div>
+                <div>
+                    <label htmlFor="password">Senha: </label>
+                    <input type="password" id="password" 
+                        name="password" placeholder="Digite sua senha"
+                        value={password}
+                        onChange={(e => setPassword(e.target.value))}
+                        required/>
                 </div>
                 <div>
                     <input type="submit" value="send"/>
