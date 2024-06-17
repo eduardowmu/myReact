59ef7327b9c81f470c3e6b1f8fc474a20ef5fb17
Eduardo Watanabe <eduardo.watanabe@vilt.local>
2024-06-16 21:23:46 -0300

    passando eventos por props

--- my-app-react/src/components/Evento.js
+++ my-app-react/src/components/Evento.js
@@ -1,16 +1,24 @@
+import Button from './Button'
+
 function Evento() {
     /**
      * Reference: https://www.devmedia.com.br/alert-em-javascript/37208
      */
     function myEvent() {
         alert("You activate this message!")
+        console.log("You activate this message!")
     }
 
+    function secondEvent() {
+        alert("Ativando meu segundo evento")
+        console.log("Ativando meu segundo evento")
+    }
 
     return(
         <>
             <p>Click here for shooting an event</p>
-            <button onClick={myEvent}>Event</button>
+            <Button event={myEvent} text="Primeiro Evento"/>
+            <Button event={secondEvent} text="Segundo Evento"/>
         </>
     )
 }
