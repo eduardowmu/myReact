71b089dc5ef70ccd80d1a52d9c1f3ead9ca87993
Eduardo Watanabe <eduardo.watanabe@vilt.local>
2024-06-26 23:27:13 -0300

    começando com a página de listagem de projetos.

--- my-proj-react/src/components/pages/Projects.js
+++ my-proj-react/src/components/pages/Projects.js
@@ -1,5 +1,8 @@
 import Message from "../layout/Message"
 import { useLocation } from "react-router-dom"
+import styles from './Projects.module.css'
+import Container from "../layout/Container"
+import LinkButton from "../layout/LinkButton"
 
 function Projects() {
     //resgatando msg através deste hook
@@ -10,9 +13,15 @@ function Projects() {
     }
     
     return(
-        <div>
-            <h1>My Projects</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>My Projects</h1>
+                <LinkButton to='/newproject' text='New Project'/>
+            </div>
             {message && <Message msg={message} type='success'/>}
+            <Container customClass='start'>
+                <p>Projetos...</p>
+            </Container>
         </div>
     )
 }
