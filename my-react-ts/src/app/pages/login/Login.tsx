f3bdbf13f59521da2bf34578f169328b06716132
Eduardo Watanabe <eduardo.watanabe@vilt.local>
2024-07-07 15:24:26 -0300

    usando useState no react ts

--- my-react-ts/src/app/pages/login/Login.tsx
+++ my-react-ts/src/app/pages/login/Login.tsx
@@ -1,21 +1,30 @@
-/**useHistory is deprecated to this version */
-import { useNavigate } from "react-router-dom"
+import { useState } from "react"
+
 
 export const Login = () => {
-    const navigate = useNavigate()
-
-    /**
-     * Este método irá fazer o papel de um
-     * redirecionamento
-     */
-    const handleClick = () => {
-        navigate('/pagina-inicial')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    
+    const handleLogin = () => {
+        console.log(email, password)
     }
-
     return(
         <div>
-            Login
-            <button onClick={handleClick}>Página Inicial</button>
+            <form>
+                <label>
+                    <span>E-mail</span>
+                    <input type="email" value={email} 
+                        onChange={e => setEmail(e.target.value)}/>
+                </label>
+                <label>
+                    <span>password</span>
+                    <input type="password" value={password} 
+                        onChange={e => setPassword(e.target.value)}/>
+                </label>
+                <button type="button" onClick={handleLogin}>
+                    Enviar
+                </button>
+            </form>
         </div>
     )
 }
