bbc2f594bd0507034cb74046dc3d42546bb2e018
Eduardo Watanabe <eduardo.watanabe@vilt.local>
2024-07-07 20:41:59 -0300

    aprendendo useEffect

--- my-react-ts/src/app/pages/login/Login.tsx
+++ my-react-ts/src/app/pages/login/Login.tsx
@@ -1,9 +1,32 @@
 import { useState } from "react"
+import { useEffect } from "react"
 
 
 export const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+
+    useEffect(() => {
+        if(window.confirm('Are tou there?')) {
+            console.log('YES')
+        } else {
+            console.log('NO')
+        }
+    /**Como ainda não temos nenhuma dependência,
+     * então o segundo parametro pode ser um array
+     * vazio. Com isso, esta "função" será executada
+     * apenas uma vez, ou seja, somente quando nosso
+     * componente for carregado uma única vez.
+     */
+    },[]);
+
+    useEffect(() => {
+        console.log(email, password)
+    /**Diferentemente da forma anterior, este useEffect
+     * será executado toda vez que estes parametros forem
+     * alterados
+     */
+    },[email, password])
     
     const handleLogin = () => {
         console.log(email, password)
