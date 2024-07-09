7b8ce35622b5849ef35d9e721f5bc3f565244e9f
Eduardo Watanabe <eduardo.watanabe@vilt.local>
2024-07-09 13:17:10 -0300

    Outra forma de usar useRef

--- my-react-ts/src/app/pages/dashboard/Dashboard.tsx
+++ my-react-ts/src/app/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,19 @@
+import { useRef } from "react"
 import { Link } from "react-router-dom"
 
 export const Dashboard = () => {
+    /**Inicializando com um objeto, com um
+     * atributo. Veremos que não será necessário
+     * fazer um SET, como fazíamos no useState
+    */
+    const counterRef = useRef({ counter: 0 })
+
     return(
         <div>
             <p>Dashboard</p>
+            <p>{counterRef.current.counter}</p>
+            <button onClick={() => counterRef.current.counter++}>Somar</button>
+            <button onClick={() => console.log(counterRef.current.counter)}>Log</button>
             <Link to='/login'>Login</Link>
         </div>
     )
