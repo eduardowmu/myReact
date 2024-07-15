7052c568ab44dc13893a1eccd74a480dd277ca8e
Eduardo Watanabe <eduardo.watanabe@vilt.local>
2024-07-14 23:47:01 -0300

    continua

--- my-react-ts/src/app/pages/dashboard/Dashboard.tsx
+++ my-react-ts/src/app/pages/dashboard/Dashboard.tsx
@@ -1,30 +1,19 @@
-import { useRef } from "react"
-import { Link } from "react-router-dom"
-import { useUsuarioLogado } from "../../shared/hooks"
+import { useState } from "react"
 
 export const Dashboard = () => {
-    const counterRef = useRef(0)
-
-    
-    /**
-     * compartilhando o UsuarioLogadoContext
-     * criado em UsuarioLogado
-     */
-    // const usuarioLogadoContext = useContext(UsuarioLogadoContext)
-
-    // também podemos compartilhar apenas o atributo
-    const { nomeDoUsuario, logout } = useUsuarioLogado()
-
+    // lista de strings
+    const [list, setList] = useState<string[]>(['Teste', 'Teste', 'Teste'])
     return(
         <div>
-            <p>Dashboard</p>
-            {/* consumindo o contexto usuarioLogadoContext*/}
-            <p>{nomeDoUsuario}</p>
-            <p>Contador: {counterRef.current}</p>
-            <button onClick={() => counterRef.current++}>Somar</button>
-            <button onClick={() => console.log(counterRef.current)}>Log</button>
-            <button onClick={logout}>Logout</button>
-            <Link to='/login'>Login</Link>
+            <p>List Dashboard</p>
+            <input
+                
+            />
+            <ul>
+                {list.map((value, index) => {
+                    return <li>{value} {index}</li>
+                })}
+            </ul>
         </div>
     )
 }
