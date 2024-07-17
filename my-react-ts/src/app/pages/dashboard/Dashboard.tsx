0af4c4bd5750fb068f6cf3d5f0107eeda81ca8a7
Eduardo Watanabe <eduardo.watanabe@vilt.local>
2024-07-17 17:51:27 -0300

    trabalhando com listas

--- my-react-ts/src/app/pages/dashboard/Dashboard.tsx
+++ my-react-ts/src/app/pages/dashboard/Dashboard.tsx
@@ -1,14 +1,33 @@
-import { useState } from "react"
+import { useCallback, useState } from "react"
 
 export const Dashboard = () => {
     // lista de strings
     const [list, setList] = useState<string[]>(['Teste', 'Teste', 'Teste'])
+    
+    /*função que ir´pa tratar algum tipo um evento de algum elemento*/
+    const handleInputKeyDown: 
+        React.KeyboardEventHandler<HTMLInputElement> = useCallback((e) => {
+        if(e.key === 'Enter') {
+            //se não tiver nada no campo, cancela
+            if(e.currentTarget.value.trim().length === 0) {return}
+            
+            /*uma forma de pegar a lista inicial com um novo
+            valor adicionado*/
+            setList([...list, e.currentTarget.value])
+
+            /*Outra forma também de se fazer com adição de 
+            conteúdo na lista*/
+            //const value = e.currentTarget.value
+            // setList((oldList) => {
+            //     return [...oldList, value]
+            // })
+        }   
+    }, [])
+
     return(
         <div>
             <p>List Dashboard</p>
-            <input
-                
-            />
+            <input onKeyDown={handleInputKeyDown}/>
             <ul>
                 {list.map((value, index) => {
                     return <li>{value} {index}</li>
